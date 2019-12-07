482b77485a7a0f5f5d44042e2b36e57098615b6f
Johannes Raufeisen <johannes.raufeisen@rwth-aachen.de>
2019-12-07 14:23:03 +0100

    Initial Commit

---
+++ LanguageParsingInAction/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LanguageParsingInAction
+//
+//  Created by Johannes on 07.12.19.
+//  Copyright © 2019 Johannes Raufeisen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
